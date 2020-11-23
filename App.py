e12692d9ec4c015904890047f9a0717c401c20a3
Richie and Robert <glennonr@moravian.edu>
2020-11-23 11:44:01 -0500

    Decoded ByteStrings

--- App.py
+++ App.py
@@ -8,13 +8,10 @@ app = Flask(__name__)
 
 @app.route('/get', methods=["GET"])
 def get_from_redis():
-    out_iplist = r.hkeys("iphash")
-    decode_list = [x.decode('utf-8') for x in out_iplist]
-    for ip in out_iplist:
-        decode_list.append(ip.decode())
-
-    out = {'ips': str(out_iplist)}
-    return out, 200
+    iplist = r.hkeys("iphash")
+    decode_list = [x.decode('utf-8') for x in iplist]
+    out = {'ips': decode_list}
+    return jsonify(out), 200
 
 
 if __name__ == '__main__':
